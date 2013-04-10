b24e896525ed19a400b745c6926e742cd225279e
kuj1pal <kuehnjoh@gmail.com>
2013-04-10 10:45:06 -0700

    first commit

---
+++ cmake/actionlib-msg-paths.cmake
@@ -0,0 +1,2 @@
+set(actionlib_MSG_INCLUDE_DIRS /opt/ros/groovy/share/actionlib/msg;/opt/ros/groovy/share/actionlib/msg;/opt/ros/groovy/share/actionlib/msg)
+set(actionlib_MSG_DEPENDENCIES actionlib_msgs;std_msgs)
